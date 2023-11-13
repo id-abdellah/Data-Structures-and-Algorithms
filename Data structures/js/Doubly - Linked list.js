44db63dc5a59ed1e3cbac7b29ccaf46cd35efc6f
Mohamed ID-ABDELLAH <master.mohamed2018@gmail.com>
2023-11-13 23:14:36 +0100

    doubly linked list

---
+++ Data structures/js/Doubly - Linked list.js
@@ -0,0 +1,105 @@
+class node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    };
+};
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    };
+
+    isEmpty() {
+        return this.size == 0;
+    };
+
+    getSize() {
+        return this.size;
+    };
+
+    prepend(value) {
+        const newNode = new node(value);
+        if (this.isEmpty()) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.size++;
+        } else {
+            newNode.next = this.head;
+            this.head.previous = newNode;
+            this.head = newNode;
+            this.size++
+        };
+    };
+
+    append(value) {
+        const newNode = new node(value);
+        if (this.isEmpty()) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.size++;
+        } else {
+            this.tail.next = newNode;
+            newNode.previous = this.tail;
+            this.tail = newNode;
+            this.size++
+        };
+    };
+
+    removeFromFront() {
+        if (this.isEmpty()) return null;
+        if (this.getSize() == 1) {
+            const value = this.head.value;
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            return value;
+        };
+        const value = this.head.value;
+        this.head = this.head.next;
+        this.head.previous = null;
+        this.size--;
+        return value;
+    };
+
+    removeFromEnd() {
+        if (this.isEmpty()) return null;
+        let value = this.tail.value;
+        if (this.getSize() == 1) {
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            return value;
+        };
+        let previousNodeOfTail = this.tail.previous;
+        previousNodeOfTail.next = null;
+        this.tail = previousNodeOfTail;
+        this.size--;
+        return value;
+    };
+
+    print() {
+        if (this.isEmpty()) return "List is empty";
+        let headNode = this.head;
+        let data = [];
+        while (headNode) {
+            data.push(headNode.value)
+            headNode = headNode.next;
+        };
+        return data;
+    };
+
+    printReverse() {
+        if (this.isEmpty()) return null;
+        let tailNode = this.tail;
+        let dataReverse = [];
+        while (tailNode) {
+            dataReverse.push(tailNode.value)
+            tailNode = tailNode.previous;
+        };
+        return dataReverse;
+    };
+};
